186d088325c15b0c2bf04951583b2428b29df121
Wmuga <cherkasovaa@mpei.ru>
2022-03-25 14:34:25 +0300

    Lab2

--- Lab2/index.js
+++ Lab2/index.js
@@ -7,24 +7,21 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false })
 const app = express()
 
 // Порт, на котором будет хостится приложение
-const port = 3000
+const port = 5000
 
 // Route на POST  по адрессу /. req.body содержит в себе тело отправленных данных, из которых и собирается ответ
-app.post('/',urlencodedParser,(req,res)=>{
-    res.send(req.body)
+app.post('/info',urlencodedParser,(req,res)=>{
+  res.render('template',{test:JSON.stringify(req.body)})
 })
+
+// Говорим приложению, что все изначально берется из директории /public в папке проекта
+app.use(express.static(__dirname + '/public'))
 
 // Говорим приложению, что рендерим html через ejs и указываем директорию шаблонов
 app.set('view engine','ejs')
 app.set('views',__dirname + '/templates')
 
-//Рендер шаблона по запросу в /
-app.get('/',(req,res)=>{
-    res.render('template',{test:'TEST DONE'})
-})
 
-// Говорим приложению, что все изначально берется из директории /public в папке проекта
-app.use(express.static(__dirname + '/public'))
 // Говорим приложению, чтобы слушал на заданном порте
 app.listen(port,()=>{
   console.log(`Working on port ${port}`)
